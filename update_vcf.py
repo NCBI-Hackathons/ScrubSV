04e058d08af133967d87e0425f75fb0ed25ce521
Steve Osazuwa <sosazuwa00@gmail.com>
2018-08-07 17:05:28 -0400

    logging change

--- update_vcf.py
+++ update_vcf.py
@@ -15,6 +15,8 @@ def _parse_args():
     parser.add_argument(
         'input_tsvs', metavar='FILES', nargs='*', help='TSVs to apply to input vcf.')
     parser.add_argument(
+        '-v', '--verbose', action='store_true', help='TSVs to apply to input vcf.')
+    parser.add_argument(
         "--output",
         type=str,
         help="output VCF file name. Defaults to prefix.scrubbed.vcf")
@@ -25,16 +27,17 @@ def _parse_args():
 if __name__ == '__main__':
     args = _parse_args()
 
-    logging.basicConfig(filename='janitorBER.log', level=logging.DEBUG, filemode='w')
+    if args.verbose:
+        logging.basicConfig(filename='janitorDEBUG.log', level=logging.DEBUG, filemode='w')
 
     # argparse
-    if args.target_vcf is None:
+    if args.output is None:
         target_vcf = "{ori_vcf_prefix}.scrubbed.vcf".format(
             ori_vcf_prefix=args.original_vcf.rstrip(".vcf"))
     else:
-        target_vcf = args.target_vcf
+        target_vcf = args.output
 
     # Combine flags
-    with vcfcombiner.concat_read_only_file_stream(args.input_tsvs) as tsv_fhs:
+    with vcfcombiner.concat_read_only_file_stream(*args.input_tsvs) as tsv_fhs:
         vcfcombiner.vcf_update(
             target_vcf, source_vcf=args.original_vcf, tsv_stream=tsv_fhs)  # TODO
