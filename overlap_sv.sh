058724c283ebc225c0f3de2f582abfbcdd8628a8
Steve Osazuwa <sosazuwa00@gmail.com>
2018-08-07 16:04:40 -0400

    minor updates. Ignore added

--- overlap_sv.sh
+++ overlap_sv.sh
@@ -1,8 +1,9 @@
+#!/usr/bin/env bash
+
 module load vcftools
 module load R
 
 #first file
-vcf-query -f '%ID\t%CHROM\t%POS\t%INFO/END\t%INFO/SVTYPE\n' HG002.bwamem.ill.mapped.filter.sort.bam.delly_noalts.vcf > HG002_delly.tab
+vcf-query -f '%ID\t%CHROM\t%POS\t%INFO/END\t%INFO/SVTYPE\n' data/H002.bwamem.ill.mapped.filter.sort.bam.lumpy_noalts.vcf > HG002_delly.tab
 Rscript overlap_sv.R --arg1=HG002_delly.tab --arg2=DEL
 Rscript overlap_sv.R --arg1=HG002_delly.tab --arg2=DUP
-
