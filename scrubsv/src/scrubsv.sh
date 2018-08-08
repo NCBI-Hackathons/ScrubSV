4acffaf307593b6c21fe0c361be84c4919a7e94f
Steve Osazuwa <sosazuwa00@gmail.com>
2018-08-08 16:17:19 -0400

    WIP applet

--- scrubsv/src/scrubsv.sh
+++ scrubsv/src/scrubsv.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
+# $1 SV VCF
+# $2 SNP VCF
+phase-haplotypes () {
+    svf="$1"
+    svf_prefix=${svf%%.vcf.gz}
+    svf_prefix=${svf_prefix%%.vcf}
+    
+    snpf="$2"
 
-assemble-haplotypes () {
-    TODO
+    hapcut_fn="${svf_prefix}.Hapcut.vcf"
+
+    bam_f="$3"
+
     # output SNP VCF w/ phasing
+    zcat <"$svf" |awk '!($10 ~ "0/3" || $10 ~ "1/4"|| $10 ~ "2/3"||$10 ~ "1/3")' > "$hapcut_fn"
+
+    ##use extractHAIRS to convert BAM file to the compact fragment file format containing only haplotype-relevant information
+    frag_fn="${svf_prefix}.fragment_file"
+
+    extractHAIRS \
+    --bam "${bam_f}" \
+    --VCF "$hapcut_fn" --out "$frag_fn"
+
 }
 
-check-overlap () {
+flag-overlap () {
     echo "This looks for overlaps between SVs"
 }
 
@@ -35,6 +54,12 @@ main() {
     dx download "$mapping_bam" -o "$mapping_bam_name"
 
 
+
+    phased_vcf="$svs_vcf_name".phased.vcf
+    phase-haplotypes "$svs_vcf_name" "$snp_vcf_name" > "$phased_vcf" &
+
+    overlap_flags_tsv="$svs_vcf_name".overlap.flag.tsv
+    flag-overlap
 
 
     flagged_vcf=$(dx upload flagged_vcf --brief)
