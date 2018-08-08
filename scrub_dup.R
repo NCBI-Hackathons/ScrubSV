35e4eab1eba2e3b4c207470217d8369f77f90870
scorpri3 <priyakrithivasan@gmail.com>
2018-08-08 12:59:26 -0400

    Script to flag duplications

---
+++ scrub_dup.R
@@ -0,0 +1,71 @@
+#This script reads in a file from the output of intersecting duplications with overlapping SNVs, and then performs the following has two parts: 
+#(i) Identify duplications that overlap a user-defined minimum number of SNPs (num_snps_sd, at least 4), and flag duplications with highly fluctuating SNP coverages (read depth SDs for a given duplication larger than 2 standard deviations from the mean)
+#(ii) Flag duplications with a minimum number (user specified, num_multiallelic_snps) of multiallelic SNPs.
+#Usage: Rscript duplication_flags.R vcffile num_multiallelic_snps num_snps_sd
+
+#load required libraries, install them if not available
+packages <- c("dplyr","tidyr","data.table","reshape2","stringr")
+package.check <- lapply(packages, FUN = function(x) {
+  if (!require(x, character.only = TRUE)) {
+    install.packages(x, dependencies = TRUE)
+    library(x, character.only = TRUE)
+  }
+})
+
+#store user arguments
+args = commandArgs(trailingOnly=TRUE)
+vcffile = args[1]
+num_snps_sd = args[2]
+num_multiallelic_snps = args[3]
+
+#read file containing SNPs overlapping with SVs
+dups <- fread(vcffile,sep="\t",header=F)
+#remove SNPs with DP 0
+dups <- dups %>% filter(dups, !(V17==0))
+
+#identifying SVs with SD of read depths greater than SD threshold (mean + 2SD)
+sd_min_numsnp_threshold = ifelse(num_snps_sd > 4, num_snps_sd, 4)
+dups_snps_n5 <- dups %>% 
+  group_by(V14) %>% 
+  summarize(nsnp=n()) %>% 
+  filter(nsnp > sd_min_numsnp_threshold)
+DP_SD <- dups %>% 
+  filter(V14 %in% dups_snps_n5$V14) %>% 
+  group_by(V14) %>% 
+  summarize(sdDP=sd(as.numeric(V17)))
+#get SD cut-off value
+DPSD_threshold <- mean(DP_SD$sdDP) + (2 * sd(DP_SD$sdDP)) 
+DP_SD_thresh <- filter(DP_SD, sdDP>DPSD_threshold)
+dups_DP_SD <- filter(dups, V14 %in% DP_SD_thresh$V14) %>% 
+  mutate(FLAG = "DP_highSD") %>% 
+  select(V14,V12,FLAG) %>% 
+  distinct()
+colnames(dups_DP_SD) <- c("ID","POS","FLAG")
+
+#write to table
+#write.table(dups_DP_SD, "dups_DP_highSD.txt",sep="\t",col.names = T,row.names = F,quote=F)
+
+#group multiallelic SNVs
+#to do: edit to allow for other multiallelic genotype combinations
+multiallelic <- c("1/2","2/1","2|1","1|2")
+
+#identify SVs overlapping multiallelic SNVs
+dups_multiallelic <- dups %>% 
+  separate(V10,sep=":",into=c("GT")) %>% 
+  filter(GT %in% multiallelic) %>% 
+  group_by(V14) %>% 
+  summarize(num_multiallelic = n()) %>% 
+  filter(num_multiallelic >= num_multiallelic_snps)
+#flag only those SVs with multiallelic SNVs that have not been flagged for high DP SD before
+dups_MA <- dups %>% 
+  filter(V14 %in% dups_multiallelic$V14 && !(V14 %in% DP_SD_thresh$V14)) %>% 
+  mutate(FLAG = "multiallelic") %>% 
+  select(V14,V12,FLAG) %>% 
+  distinct()
+colnames(dups_MA) <- c("ID","POS","FLAG")
+
+#write to table
+#write.table(dups_MA, "dups_multiallelic_SNPs.txt", sep = "\t", col.names = T, row.names = F, quote = F)
+
+flagged_duplications <- rbind(dups_DP_SD, dups_MA)
+write.table(flagged_duplications, "flagged_duplications.txt", sep = "\t", col.names = T, row.names = F, quote = F)
