b0e79cd5ee34c2a8998e14de4403e448f8eee9a7
Steve Osazuwa <sosazuwa00@gmail.com>
2018-08-08 12:22:21 -0400

    script cleanup

--- overlap_sv.sh
+++ overlap_sv.sh
@@ -2,12 +2,25 @@
 
 ################ Requirements: Installation of vcftools, R, and GenomicRanges (R package)
 
-###### At DNANexus docker
+###### On DNANexus worker
 ### vcftools:   aps install vcftools
 ### R is installed already, just installed "GenomicRanges" 
 ###             source("https://bioconductor.org/biocLite.R")
 ###             biocLite("GenomicRanges")
 
-vcf-query -f '%ID\t%CHROM\t%POS\t%INFO/END\t%INFO/SVTYPE\n' data/H002.bwamem.ill.mapped.filter.sort.bam.lumpy_noalts.vcf > HG002_delly.tab
-Rscript overlap_sv.R --arg1=HG002_delly.tab --arg2=DEL
-Rscript overlap_sv.R --arg1=HG002_delly.tab --arg2=DUP
+
+################ Description ################
+# This script detects overlays in SV falls and flags them in the output tabular
+
+
+################  The script ################
+
+sv_vcf="${1}"
+sv_vcf_name= "${sv_vcf%%.vcf}"
+sv_vcf_name="${sv_vcf%%.vcf.gz}"
+sv_vcf_name="${sv_vcf##*/}"
+tabular_filename="${sv_vcf_name}.gz"
+
+vcf-query -f '%ID\t%CHROM\t%POS\t%INFO/END\t%INFO/SVTYPE\n' "${sv_vcf}" > "${tabular_filename}"
+Rscript overlap_sv.R --arg1="${tabular_filename}" --arg2=DEL
+Rscript overlap_sv.R --arg1="${tabular_filename}" --arg2=DUP
