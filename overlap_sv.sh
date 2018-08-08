eb242cb73852d5acc558e1e81f24d75ac725e919
root <root@job-FJbPy280p9vY1yp3Kq9kGQ7P.dnanex.us>
2018-08-08 15:28:37 +0000

    updated overlap_sv.sh after confirmation at clean environment at DNAnexsus worker

--- overlap_sv.sh
+++ overlap_sv.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-module load vcftools
-module load R
+################ Requirements: Installation of vcftools, R, and GenomicRanges (R package)
 
-#first file
+###### At DNANexus docker
+### vcftools:   aps install vcftools
+### R is installed already, just installed "GenomicRanges" 
+###             source("https://bioconductor.org/biocLite.R")
+###             biocLite("GenomicRanges")
+
 vcf-query -f '%ID\t%CHROM\t%POS\t%INFO/END\t%INFO/SVTYPE\n' data/H002.bwamem.ill.mapped.filter.sort.bam.lumpy_noalts.vcf > HG002_delly.tab
 Rscript overlap_sv.R --arg1=HG002_delly.tab --arg2=DEL
 Rscript overlap_sv.R --arg1=HG002_delly.tab --arg2=DUP
